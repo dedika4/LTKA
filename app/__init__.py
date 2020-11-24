2779100d8ddc5f9bac662234e69904d91137e1ab
dedika4 <dewamahardika499@gmail.com>
2020-11-24 13:29:37 +0000

    update routes display data dari arduino

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+	app.run(debug=True, port=80, host='0.0.0.0')
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,19 @@
-from flask import render_template
-from app import app
-from app.data import positionData
+from flask import Flask, render_template
+import serial
+
+app = Flask(__name__)
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Dewa Mahardika'}
-    pos = positionData()
-    pos.getDistance()
-    pos.getDirection()
-    return render_template('index.html', title='Home', user=user, data=pos)
+	ser.flush()
+	if ser.in_waiting > 0:
+		line = ser.readline().decode('utf-8').rstrip()
+	else:
+		line = 0
+	return render_template('index.html', title='Home', user=line)
+
+if __name__ == '__main__':
+	app.run(debug=True, port=80, host='0.0.0.0')
